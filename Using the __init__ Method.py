5c11455c723ee79f5437968b8813f1157e32053b
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 14:02:36 +0530

    Create  Using the __init__ Method.py

---
+++ Using the __init__ Method.py
@@ -0,0 +1,8 @@
+# 2. Using the __init__ Method
+# Already implemented in Car class
+
+# 3. Creating Multiple Objects
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
+car1.display()
+car2.display()
