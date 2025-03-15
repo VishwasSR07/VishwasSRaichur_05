a3062c1aee0953820c447c9b55a45950e0f2a5b6
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 12:39:53 +0530

    Create OOP-based Calculator.py

---
+++ OOP-based Calculator.py
@@ -0,0 +1,15 @@
+# 24. OOP-based Calculator
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
