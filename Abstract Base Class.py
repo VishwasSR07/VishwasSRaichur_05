eb1327299cbdce5490603e4779cf7202653417e1
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 12:38:33 +0530

    Create  Abstract Base Class.py

---
+++ Abstract Base Class.py
@@ -0,0 +1,12 @@
+# 18. Abstract Base Class
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius * self.radius
